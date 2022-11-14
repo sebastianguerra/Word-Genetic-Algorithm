af71c41a648c78a56d2df937abfafde44812438c
sebastianguerra <spguerraespinoza@gmail.com>
2022-11-13 21:28:45 -0300

    Colores :D

--- main.c
+++ main.c
@@ -6,7 +6,27 @@
 
 #define MAX_WORD_LENGTH 500
 #define N_MAX 10000
-#define KEEP_BEST 0
+#define KEEP_BEST false
+
+
+#define RESET_COLOR "\x1b[0m"
+#define NEGRO_T     "\x1b[30m"
+#define NEGRO_F     "\x1b[40m"
+#define ROJO_T      "\x1b[31m"
+#define ROJO_F      "\x1b[41m"
+#define VERDE_T     "\x1b[32m"
+#define VERDE_F     "\x1b[42m"
+#define AMARILLO_T  "\x1b[33m"
+#define AMARILLO_F  "\x1b[43m"
+#define AZUL_T      "\x1b[34m"
+#define AZUL_F      "\x1b[44m"
+#define MAGENTA_T   "\x1b[35m"
+#define MAGENTA_F   "\x1b[45m"
+#define CYAN_T      "\x1b[36m"
+#define CYAN_F      "\x1b[46m"
+#define BLANCO_T    "\x1b[37m"
+#define BLANCO_F    "\x1b[47m"
+
 
 const char alfabeto[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789 ,.!?():;<>[]{}-+*/%&|^~$#@";
 
@@ -79,7 +99,11 @@ int main() {
         strcpy(mejor, mejorT);
 
         // Muestra la mejor palabra
-        printf("%d, puntaje: %d. %s\n", i, puntaje_palabra, mejor);
+        printf(
+            AMARILLO_T "%d" AZUL_T " puntaje:"VERDE_T" %d" BLANCO_T "\t%s\n" RESET_COLOR, 
+            i, 
+            puntaje_palabra, 
+            mejor);
 
 
         if(strcmp(mejor, palabra) == 0) {
